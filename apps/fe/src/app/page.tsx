504e7748f2c0b10ec3dba2128414c0d27ccb95ce
fpalmagil <fpalmagil@jairosoft.com>
2024-10-14 14:10:21 +0800

    button and alert test functions

--- apps/fe/src/app/page.tsx
+++ apps/fe/src/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import Button from "@repo/ui/button";
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1>fe</h1>
-      <Button className="bg-blue-500 text-white" text="fe button" />
+      <Button  alertText={() => alert("This is from FE")}  // Pass a function
+  // Passing the function here
+ className="bg-blue-500 text-white" text="fe button" />
     </main>
   );
 }
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,8 +1,14 @@
+"use client"; // Add this line to make this a Client Component
+
 import { cn } from "./utils";
 
-const Button = ({ className, text }: { className: string; text: string }) => {
+const Button = ({ className, text, alertText }: { className: string; text: string;   alertText?: () => void; // Ensure this is a function type
+}) => {
   return (
-    <div className={cn("bg-green-500 text-white", { className })}>{text}</div>
+    <div   onClick={() => { 
+      if (alertText) alertText(); // Call the function when clicked
+    }}// You directly pass the function, no need for an arrow function if it's just invoking the function.
+    className={cn("bg-green-500 text-white", { className })}>{text}</div>
   );
 };
 

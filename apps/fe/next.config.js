7d45d97a4561dcfc328d40887f38fcad5cb948fe
kenjanjan <kjliloc@hcdc.edu.ph>
2024-10-16 11:32:31 +0800

    updated

--- apps/fe/next.config.js
+++ apps/fe/next.config.js
@@ -3,7 +3,5 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
   output: "standalone",
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-  },
+
 };
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,7 +3,4 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
   output: "standalone",
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-  },
 };
